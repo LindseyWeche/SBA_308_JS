8d0c76af3635edca25b139659deb54cd4501eb64
Lindsey Weche <lindseyhaishanw22@icloud.com>
2024-04-28 20:09:46 -0400

    totalPoints

---
+++ JS/app.js
@@ -0,0 +1,113 @@
+const CourseInfo = {
+    id: 301,
+    name: "Introduction to Cultural Psychology",
+};
+
+const AssignmentGroup = {
+    id: 10177,
+    name: "Fundamentals of JavaScript",
+    course_id: 301,
+    group_weight: 25,
+    assignments: [
+      {
+        id: 1,
+        name: "Haitian History",
+        due_at: "2023-01-25",
+        points_possible: 50,
+      },
+      {
+        id: 2,
+        name: "Cultural Anthropology",
+        due_at: "2023-03-20",
+        points_possible: 100,
+      },
+      {
+        id: 3,
+        name: "Superstition & Schema",
+        due_at: "2023-04-26",
+        points_possible: 150,
+      },
+      {
+        id: 4,
+        name: "Religion vs Beliefs",
+        due_at: "3156-11-15",
+        points_possible: 500,
+      }
+    ]
+  };
+ 
+
+const newPoints = AssignmentGroup.assignments.map((point) => point.points_possible)
+ const TotalnewPoints = newPoints.reduce((acc, curr) => acc + curr)
+ console.log ("The Total of Points for these four modules is :",TotalnewPoints)
+//  Apply map method to have a new array for all the points_possible in newPoints and reduce to have a single number (the sum)
+
+  // The provided learner submission data.
+// const LearnerSubmissions = [
+//     {
+//       learner_id: 125,
+//       assignment_id: 1,
+//       submission: {
+//         submitted_at: "2023-01-25",
+//         score: 47
+//       }
+//     },
+//     {
+//       learner_id: 125,
+//       assignment_id: 2,
+//       submission: {
+//         submitted_at: "2023-02-12",
+//         score: 150
+//       }
+//     },
+//     {
+//       learner_id: 125,
+//       assignment_id: 3,
+//       submission: {
+//         submitted_at: "2023-01-25",
+//         score: 400
+//       }
+//     },
+//     {
+//       learner_id: 132,
+//       assignment_id: 1,
+//       submission: {
+//         submitted_at: "2023-01-24",
+//         score: 39
+//       }
+//     },
+//     {
+//       learner_id: 132,
+//       assignment_id: 2,
+//       submission: {
+//         submitted_at: "2023-03-07",
+//         score: 140
+//       }
+//     }
+//   ];
+  
+//   function getLearnerData(course, ag, submissions) {
+//     // here, we would process this data to achieve the desired result.
+//     const result = [
+//       {
+//         id: 125,
+//         avg: 0.985, // (47 + 150) / (50 + 150)
+//         1: 0.94, // 47 / 50
+//         2: 1.0 // 150 / 150
+//       },
+//       {
+//         id: 132,
+//         avg: 0.82, // (39 + 125) / (50 + 150)
+//         1: 0.78, // 39 / 50
+//         2: 0.833 // late: (140 - 15) / 150
+//       }
+//     ];
+  
+//     return result;
+//   }
+  
+//   const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
+  
+//   console.log(result);
+  
+  
